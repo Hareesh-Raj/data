467654f9d15dfecf6f92851d654aac772fff0fd3
Hareesh-Raj <125631814+Hareesh-Raj@users.noreply.github.com>
2023-04-05 14:03:38 +0530

    Review Comments Exercise 2

--- Exercise2/day2.sql
+++ Exercise2/day2.sql
@@ -1,67 +1,60 @@
 -- Question 1 
 -- Write a SQL query to remove the details of an employee whose first name ends in ‘even’
-delete from employees where first_name like '%even';
-select * from employees where first_name like '%even';
+-- stEVEN,STEvEn.
+select * from employees where lower(first_name) like '%even';
+delete from employees where lower(first_name) like '%even';
 -- Question 2
 -- Write a query in SQL to show the three minimum values of the salary from the table.
 select * from employees order by salary limit 3;
 -- Question 3
 -- Write a SQL query to copy the details of this table into a new table with table name as Employee table and to delete the records in employees table.
-create table Employee (
-	employee_id INT  NOT NULL,
-	first_name VARCHAR(20),
-	last_name VARCHAR(25) NOT NULL,
-	email VARCHAR(25) NOT NULL,
-	phone_number VARCHAR(20),
-	hire_date DATE NOT NULL,
-	job_id VARCHAR(10) NOT NULL,
-	salary DECIMAL(8, 2) NOT NULL,
-	commission_pct DECIMAL(2, 2),
-	manager_id INT ,
-	department_id INT  ,
-	PRIMARY KEY (employee_id)
-	);
-insert into Employee select * from employees;
+create table EMPLOYEE as (select * from employees);
 truncate table employees;
 select * from EMPLOYEE;
 -- Question 4
 -- Write a SQL query to remove the column Age from the table
 alter table employee drop column age;
+alter table employee add age int;
 -- Question 5
 -- Obtain the list of employees (their full name, email, hire_year) where they have joined the firm before 2000
 select concat(first_name,' ',last_name) as FullName,email,year(hire_date) as hire_year from employee where (year(hire_date)<2000);
+-- SELECT CONCAT_WS(' ', first_name, last_name);
 -- Question 6
 -- Fetch the employee_id and job_id of those employees whose start year lies in the range of 1990 and 1999
-select employee_id,job_id,year(hire_date) as hire_year from employee where year(hire_date) between 1990 and 1999;
+select employee_id,job_id,year(hire_date) as hire_year from employee where year(hire_date) between 1990 and 1999 and year(hire_date) not in (1990,1999);
+-- select * from job_history;
 -- Question 7
 -- Find the first occurrence of the letter 'A' in each employees Email ID Return the employee_id, email id and the letter position
-select employee_id,email,position('A',email)as letter_position from employee;
+select employee_id,email,position('A',email)as letter_position from employee where letter_position > 0;
 -- Question 8
 -- Fetch the list of employees(Employee_id, full name, email) whose full name holds characters less than 12
 select employee_id,concat(first_name,' ',last_name) as FullName,email from employee where length(concat(first_name,last_name)) < 12;
 -- Question 9
 -- Create a unique string by hyphenating the first name, last name , and email of the employees to obtain a new field named UNQ_ID Return the employee_id, and their corresponding UNQ_ID;
-select email,concat(first_name,'-',last_name,'-',email) as UNQ_ID from employee;
+select email,concat(first_name,'@',last_name,'@',email) as UNQ_ID from employee;
 -- Question 10
 -- Write a SQL query to update the size of email column to 30
 alter table employee modify email varchar(30); 
 desc table employee;
 -- Question 11
 -- Write a SQL query to change the location of Diana to London
-update departments set location_id=2400 where department_id in (select department_id from employee where first_name='Diana');
+select * from (departments d join employee e on (d.department_id=(select department_id from employee where first_name='Diana') and e.first_name='Diana')) ;
+update departments set location_id=2400 where department_id in (select department_id from employee where first_name='Diana') ;
 select department_id from employee where first_name='Diana';
 select location_id from departments where department_id=60;
 -- Question 12
 -- Fetch all employees with their first name , email , phone (without
 -- extension part) and extension (just the extension)
 -- Info : this mean you need to separate phone into 2 parts
--- eg: 123.123.1234.12345 => 123.123.1234 and 12345 . first half in phone column and second half in extension column
-select first_name,email,substr(phone_number,0,length(phone_number)-length(split_part(phone_number,'.',4))-1) as phone_column,split_part(phone_number,'.',4) as extension_column from employee;
+-- eg: 123.123.1234.12345.235 => 123.123.1234 and 12345 . first half in phone column and second half in extension column
+select first_name,
+email,
+substr(phone_number,0,length(phone_number)-length(split_part(phone_number,'.',-1))-1) as phone_column,split_part(phone_number,'.',-1) as extension_column from employee;
 -- Question 13
 -- Write a SQL query to find the employee with second and third maximum
 -- salary with and without using top/limit keyword
-select salary from (select  row_number() over(ORDER BY salary desc) as rownum,salary from (select distinct salary from employee)) where rownum between 2 and 3;
 select distinct salary from employee order by salary desc limit 3;
+select * from (select employee_id, salary, dense_rank() over(order by salary desc) as salary_rank from employee) max_salary where max_salary.salary_rank in (2,3);  
 -- Question 14
 -- Fetch all details of top 3 highly paid employees who are in department
 -- Shipping and IT
@@ -86,12 +79,14 @@ select first_name,concat(dayname(hire_date),', ',monthname(hire_date),' ',day(hi
 
 select * from jobs;
 delete from jobs where job_id = 'DT_ENGG';
+
 start transaction;
 ALTER SESSION SET AUTOCOMMIT = false;
 insert into jobs values ('DT_ENGG','Data Engineer',12000,30000);
+savepoint intial;
 commit;
 update jobs set max_salary=40000 where job_id='DT_ENGG';
-rollback;
+rollback to intial;
 ALTER SESSION UNSET AUTOCOMMIT;
 -- Question 18
 -- Find the average salary of all the employees who got hired after 8th January 1996 but before 1st January 2000 and round the result to 3 decimals
